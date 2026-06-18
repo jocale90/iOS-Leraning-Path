e7b6ca6df000db94274f89293ae94ebe349f8bd0
japerniac <japerniac@falabella.cl>
2026-06-18 19:49:27 -0400

    Initial Commit

---
+++ Leaarning-Path/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Leaarning-Path
+//
+//  Created by Jose Pernia on 18-06-26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Leaarning-PathUITests/Leaarning_PathUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Leaarning_PathUITestsLaunchTests.swift
+//  Leaarning-PathUITests
+//
+//  Created by Jose Pernia on 18-06-26.
+//
+
+import XCTest
+
+final class Leaarning_PathUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
